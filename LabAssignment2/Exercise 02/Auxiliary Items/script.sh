02039102ed6acd41473501b27d73217767310a89
FilipeAz <filipepazevedo@gmail.com>
2020-09-28 17:06:20 +0100

    Added Lab Assignment 2 files

---
+++ LabAssignment2/Exercise 02/Auxiliary Items/script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dd skip=12278272 count=19968 if=usb-image.dd bs=1 > 2003_document.doc
+
+dd skip=164352 count=8037267 if=usb-image.dd bs=1 > domopers.wmv
+
+dd skip=8202752 count=318895 if=usb-image.dd bs=1 > enterprise.wav
+
+dd skip=8522240 count=24367 if=usb-image.dd bs=1 > haxor2.jpg
+
+dd skip=8546816 count=23040 if=usb-image.dd bs=1 > holly.xls
+
+dd skip=8571392 count=1399508 if=usb-image.dd bs=1 > lin_12.pdf
+
+dd skip=9972224 count=122434 if=usb-image.dd bs=1 > nlin_14.pdf
+
+dd skip=10095104 count=29885 if=usb-image.dd bs=1 > paul.jpg
+
+dd skip=10125824 count=444314 if=usb-image.dd bs=1 > pumpkin.jpg
+
+dd skip=10570240 count=99298 if=usb-image.dd bs=1 > shark.jpg
+
+dd skip=10670592 count=5498 if=usb-image.dd bs=1 > sm1.gif
+
+dd skip=10676736 count=550653 if=usb-image.dd bs=1 > surf.mov
+
+dd skip=11227648 count=1032914 if=usb-image.dd bs=1 > surf.wmv
+
+dd skip=12265984 count=11264 if=usb-image.dd bs=1 > test.ppt
+
+dd skip=12278272 count=78899 if=usb-image.dd bs=1 > wword60t.zip
